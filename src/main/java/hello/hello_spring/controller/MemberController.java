93e825272d5b1b1d9e4cbccde73f3d23970c7eca
dopiasd <zip17430@icloud.com>
2024-10-11 23:39:00 +0900

    6주차

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,7 +1,11 @@
 package hello.hello_spring.controller;
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,4 +17,25 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+   @PostMapping("/members/new")
+    public String create(MemberForm form){
+       Member member = new Member();
+       member.setName(form.getName());
+
+       memberService.join(member);
+
+       return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("member",members);
+        return "members/memberList";
+    }
 }
+
